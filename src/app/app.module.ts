8098618a787feded880edf72801b3350466c26ef
Herczog Attila <herczogattil@gmail.com>
2024-04-18 21:42:08 +0200

    update packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -12,8 +11,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     BrowserModule,
-    FormsModule,
-    NgbModule.forRoot()
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
